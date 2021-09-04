d6bc598e9635cf830ce47119cd1e9ed808f3f8e2
ch010060 <ch010060@gmail.com>
2021-09-04 14:55:45 +0800

    Redirect logging to file.

--- saucebot/log.py
+++ saucebot/log.py
@@ -1,5 +1,6 @@
 # Set up logging
 import logging
+import sys
 
 import sentry_sdk
 
@@ -7,6 +8,9 @@ from saucebot.config import config
 
 logLevel = getattr(logging, str(config.get('Bot', 'log_level', fallback='ERROR')).upper())
 logFormat = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
+
+handler = logging.FileHandler(config.get('Bot', 'log_dir', fallback='ERROR'), 'w', 'utf-8')
+handler.setFormatter(logFormat)
 
 log = logging.getLogger('saucebot')
 log.setLevel(logLevel)
@@ -16,12 +20,11 @@ log.setLevel(logLevel)
 query_log = logging.getLogger('pony.orm.sql')
 query_log.setLevel(logLevel)
 
-ch = logging.StreamHandler()
-ch.setLevel(logLevel)
-ch.setFormatter(logFormat)
+query_log.addHandler(handler)
+log.addHandler(handler)
 
-log.addHandler(ch)
-query_log.addHandler(ch)
+sys.stderr.write = log.error
+sys.stdout.write = log.info
 
 # Unless you're running your own custom fork of saucebot, you probably don't need this.
 if config.has_option('Bot', 'sentry_logging') and config.getboolean('Bot', 'sentry_logging'):
