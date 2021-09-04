7a83366ec84ca4b662980fab648861ad60770211
ch010060 <ch010060@gmail.com>
2021-09-04 14:52:38 +0800

    Change asset images.

--- saucebot/assets.py
+++ saucebot/assets.py
@@ -1,4 +1,4 @@
-AVATAR_CONFIDENT = "https://raw.githubusercontent.com/FujiMakoto/saucebot/master/assets/confident.png"
-AVATAR_SILLY = "https://raw.githubusercontent.com/FujiMakoto/saucebot/master/assets/silly.png"
-AVATAR_THINKING = "https://raw.githubusercontent.com/FujiMakoto/saucebot/master/assets/thinking.png"
+AVATAR_CONFIDENT = "https://cdn.discordapp.com/attachments/881168385507999798/883470256989941760/chino.png"
+AVATAR_SILLY = "https://cdn.discordapp.com/attachments/881168385507999798/883469438878363699/thinking.png"
+AVATAR_THINKING = "https://cdn.discordapp.com/attachments/881168385507999798/883470882339700786/notfound.png"
 ICON_FOOTER = "https://raw.githubusercontent.com/FujiMakoto/saucebot/master/assets/footer_icon.png"
